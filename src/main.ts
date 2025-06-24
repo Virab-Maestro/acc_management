14cd0484929f739cbaae2d39daaede35e67c4cad
Virab-Maestro <khachatryanvirab7@gmail.com>
2025-06-24 21:51:25 +0300

    init: vue + ts + pinia + vuetify

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import './assets/main.css'
+
+//vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import App from './App.vue'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
+createApp(App)
+    .use(createPinia())
+    .use(vuetify)
+    .mount('#app')
